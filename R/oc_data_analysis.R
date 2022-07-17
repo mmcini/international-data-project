c1290bed49e7ffb6967a39d8fdbc27ef91c10b88
mmcini <mrcl.mancini@gmail.com>
2022-07-17 18:19:45 -0300

    Added PXRF histograms

--- R/oc_data_analysis.R
+++ R/oc_data_analysis.R
@@ -2,14 +2,15 @@
 
 library(tidyverse)
 library(prospectr)
+library(extrafont)
+library(ggpubr)
 library(readxl)
 
-# Merging Vis-NIR data　############################################################################
+# Merging Vis-NIR data/fixing format　##############################################################
 
 ## Some data from Brazil has 2151 rows
 ## but most data was binned in intervals of 10 (350, 360 ... 2500)
-unbinned_oc_data <- read_excel("../data/unbinned_oc_data.xlsx", na = "NA")
-
+unbinned_oc_data <- read_excel("../data/_unbinned_oc_data.xlsx", na = "NA")
 unbinned_oc_spectra <- unbinned_oc_data %>%
                        select(c("355":"2500"))
 
@@ -21,9 +22,44 @@ binned_oc_spectra <- binned_oc_spectra[-215] %>% # removing band 2498 and replac
                      add_column("350" = unbinned_oc_data$`350`, .before = "360") %>%
                      add_column("2500" = unbinned_oc_data$`2500`)
 
-write_excel_csv(binned_oc_spectra, "../data/_binned_oc_data.csv")
+write_csv(binned_oc_spectra, "../data/_binned_oc_data.csv")
 
+# Plot layouts #####################################################################################
 
-raw_oc_data <- read_excel("../data/oc_data.xlsx")
+## Lengthy and repetitive plot formatting and functions are defined here
 
-head(raw_oc_data)
+## histograms
+hist_layout <- list(theme_bw(), ylab("Count"),
+                    theme(text = element_text(family = "Times New Roman"),
+                          legend.title = element_blank()))
+create_histograms <- function(data = NULL, variables = NULL, group_by = NULL) {
+  if (is.null(variables)) {
+    variables <- as.character(colnames(data))
+  } else {
+    variables <- data %>%
+                select(variables) %>%
+                colnames()
+  }
+  plots <- list()
+  for (i in seq_len(length(variables))) {
+    plots[[i]] <- ggplot(data, aes_string(x = variables[i], fill = group_by)) +
+                  geom_histogram() + hist_layout
+  }
+  return(plots)
+}
+
+# Descriptive stats　###############################################################################
+
+raw_oc_data <- read_excel("../data/oc_data.xlsx", na = "NA")
+
+## All countries ###################################################################################
+##OC plots
+ggplot(raw_oc_data, aes(x = OC, fill = country)) +
+  geom_histogram() + hist_layout
+
+## PXRF plots
+pxrf_data <- raw_oc_data %>%
+             select(country, c(K:Pb))
+
+pxrf_hists <- pxrf_histograms(pxrf_data, c(2:17), "country")
+ggarrange(plotlist = pxrf_hists, ncol = 4, nrow = 4, common.legend = T, legend = "bottom")
